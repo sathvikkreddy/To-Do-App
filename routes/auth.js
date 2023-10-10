386f09551b1d7ff85a20714881bb606d1d89b484
sathvik reddy <sathvikreddy8685@gmail.com>
2023-10-10 16:04:53 +0530

    commited upto login route

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const validateRegisterInput = require("../validation/registerValidation")
-
+const jwt = require("jsonwebtoken");
 
 
 // @route  GET /api/auth/test
@@ -14,7 +14,7 @@ router.get("/test", (req, res) => {
 });
 
 
-// @route  GET /api/auth/register
+// @route  POST /api/auth/register
 // @desc   create new user
 // @access Public
 router.post("/register", async (req, res) => {
@@ -55,5 +55,51 @@ router.post("/register", async (req, res) => {
         res.status(500).send(err.message)
     }
 });
+
+
+// @route  POST /api/auth/login
+// @desc   Login user
+// @access Public
+
+router.post("/login", async (req, res) => {
+    try{
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i") 
+        });
+
+        if(!user){
+            return res.status(400).json("Error while logging in");
+        }
+
+        const passwordsMatched = await bcrypt.compare(req.body.password, user.password);
+        
+        if(!passwordsMatched){
+            return res.status(400).json("Error while logging in");
+        }
+
+        const payload = {userId: user._id}
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        });
+
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60* 60* 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        });
+
+        const userToReturn = { ...user._doc };
+        delete userToReturn.password;
+
+        return res.json({
+            token: token,
+            user: userToReturn
+        });
+    }
+    catch(err){
+        console.log(err);
+        return res.status(400).send(err.message);
+    }
+})
 
 module.exports = router;
--- test.js
+++
@@ -1,5 +0,0 @@
-const isEmpty = require("./validation/isEmpty");
-
-errors={};
-
-console.log(isEmpty(errors));
